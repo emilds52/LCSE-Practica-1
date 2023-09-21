d2ccc35375fbb787de959c11b5b643dfb15e8131
vml312 <valentinmtnz99@gmail.com>
2023-09-21 16:16:49 +0200

    RX vacío

---
+++ P_RS232.srcs/sources_1/new/RS232_RX.vhd
@@ -0,0 +1,17 @@
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+use IEEE.NUMERIC_STD.ALL;
+
+
+entity RS232_RX is
+--  Port ( );
+end RS232_RX;
+
+architecture Behavioral of RS232_RX is
+
+begin
+
+
+end Behavioral;
