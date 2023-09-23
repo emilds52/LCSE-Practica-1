adca0fa04e6a0fc7965d22cacd31527f428b1d3c
vml312 <valentinmtnz99@gmail.com>
2023-09-23 22:07:11 +0200

    RS232_RX, testbench
    Está TX en el testbench de entrada de RX para ahorrar tener que ir poniendo 0 o 1 en el bit de start o stop, creo que así estaría bien pero también se podría probar sin usar TX. En cuanto a RS232_RX se ha seguido el diagrama que hicimos (diagrama_RX), salvo alguna asignación que eran comunes a varias condiciones y algún if común para no repetir código.

--- P_RS232.srcs/sources_1/new/RS232_RX.vhd
+++ P_RS232.srcs/sources_1/new/RS232_RX.vhd
@@ -18,12 +18,12 @@ end RS232_RX;
 
 architecture Behavioral of RS232_RX is
     -- Salidas
-    Valid_reg : std_logic;
-    Code_reg  : std_logic;
-    Store_reg : std_logic;
-    Valid_tmp : std_logic;
-    Code_tmp  : std_logic;
-    Store_tmp : std_logic;
+    signal Valid_reg : std_logic;
+    signal Code_reg  : std_logic;
+    signal Store_reg : std_logic;
+    signal Valid_tmp : std_logic;
+    signal Code_tmp  : std_logic;
+    signal Store_tmp : std_logic;
     -- Internos
     constant pulse_width     : unsigned(7 downto 0):=to_unsigned(174,8);
     signal   bitCounter_reg  : unsigned(7 downto 0);
@@ -39,18 +39,60 @@ architecture Behavioral of RS232_RX is
 
     begin
 
-    comb:process(all)
+    comb:process(LineRD_in, current_state_reg, reset, bitCounter_reg, stopFlag_reg, data_count_reg) --process(all) da error de sintaxis no soportado desde 1076-2008
     begin
         bitCounter_tmp <= (others => '0');
-        data_count_tmp <= (others => '0');
-        stopFlag_tmp <= '0';
+        data_count_tmp <= data_count_tmp;
+        stopFlag_tmp <= stopFlag_tmp;
+        Valid_tmp <= '0';
+        Code_tmp <= '0';
+        Store_tmp <= '0';
+        
         case(current_state_reg) is
         
             when idle =>
-                
+            data_count_tmp <= (others => '0');
+            stopFlag_tmp <= '0';
+                if LineRD_in='0' then
+                    next_state <= StartBit;
+                end if;
             
+            when StartBit =>
+                if bitCounter_tmp = pulse_width/2 then
+                    bitCounter_tmp <= (others=>'0');
+                    next_state <= RcvData;
+                else 
+                    bitCounter_tmp <= bitCounter_tmp + 1;
+                end if;  
+            
+            when RcvData =>
+                if bitCounter_tmp=pulse_width then
+                    Valid_tmp <= '1';
+                    Code_tmp <= LineRD_in;
+                    if data_count_tmp /= to_unsigned(7,data_count_tmp'length) then
+                        bitCounter_tmp <= (others=>'0');
+                        data_count_tmp <= data_count_tmp +1;
+                    else
+                        next_state <= StopBit;
+                    end if;
+                else
+                    bitCounter_tmp <= bitCounter_tmp + 1;   
+                end if;
+                    
+            when StopBit =>
+                if bitCounter_tmp = pulse_width and stopFlag_tmp='0' then
+                    Store_tmp <= LineRD_in;
+                    bitCounter_tmp <= (others=>'0');
+                    stopFlag_tmp <= '1';
+                elsif bitCounter_tmp = pulse_width/2 and stopFlag_tmp='1' then
+                    data_count_tmp <= (others => '0');
+                    bitCounter_tmp <= (others => '0');
+                    stopFlag_tmp <= '0';
+                    next_state <= idle;
+                else 
+                    bitCounter_tmp <= bitCounter_tmp + 1;   
+                end if;
             when others =>
-        
         end case ;
     end process;
 
@@ -63,10 +105,19 @@ architecture Behavioral of RS232_RX is
             Valid_reg      <= '0';
             Code_reg       <= '0';
             Store_reg      <= '0';
+        elsif rising_edge(clk) then
+            bitCounter_reg <= bitCounter_tmp;
+            data_count_reg <= data_count_tmp;
+            stopFlag_reg <= stopFlag_tmp;
+            Valid_reg <= Valid_tmp;
+            Code_reg <= Code_tmp;
+            Store_reg <= Store_tmp;
+            current_state_reg <= next_state;
         end if ;
     end process;
-
+    
     Valid_out <= Valid_reg;
     Code_out  <= Code_reg;
     Store_out <= Store_reg;
+
 end Behavioral;
