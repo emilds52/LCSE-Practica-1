23c67a3ad5ecd91757e448ffd306817c54adef95
emilds52 <emilds@live.com>
2023-10-04 16:46:32 +0200

    Changed from using a 10 bit reg to using a multiplexor with preset values, speed logic should be more or less finished, number of bits not started.

--- P_RS232.srcs/sources_1/new/RS232_TX.vhd
+++ P_RS232.srcs/sources_1/new/RS232_TX.vhd
@@ -2,6 +2,7 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
+use utility.ALL;
 
 entity RS232_TX is
     port (
@@ -9,7 +10,7 @@ entity RS232_TX is
       Reset  : in  std_logic;
       Start  : in  std_logic;
       Data   : in  std_logic_vector(7 downto 0);
-      Speed  : in  std_logic_vector(1 downto 0);
+      Speed  : in  speed_t;
       N_bits : in  std_logic_vector(1 downto 0);
       EOT    : out std_logic;
       TX     : out std_logic
@@ -24,10 +25,11 @@ signal EOT_reg : std_logic;
 type state is (idle, StartBit, SendData, StopBit);
 signal current_state_reg : state;
 
-type speed_t is (quarter, half, normal, doble);
+signal Speed_reg : speed_t;
+
 constant pulse_width_default : unsigned(7 downto 0):=to_unsigned(174,8); -- si ponemos nombre a 174 queda m�s entendible, y podemos hacer ceil(log2(174 + 1)) para obtener 7 sin que sea un "n�mero magico" que no se sabe de d�nde viene. No es muy importante pero puede ser algo que se puede hacer en las mejoras si te apetece, igual nos hace sacar una nota mejor.
-signal pulse_width_reg : unsigned(10 downto 0); -- TODO hazlo más generico
-signal pulse_count_reg : unsigned(7 downto 0);
+signal pulse_width     : unsigned(9 downto 0); -- 10 bits para poder representar 174*4 - 1 TODO: Hazlo genérico
+signal pulse_count_reg : unsigned(9 downto 0);
 signal data_count_reg  : unsigned(2 downto 0);
 
 begin
@@ -37,29 +39,12 @@ begin
         if reset = '0' then
             TX_reg            <= '1';
             current_state_reg <= idle;
-            pulse_width_reg   <= pulse_width_default - 1;
+            Speed_reg         <= normal;
         else
             case current_state_reg is
                 when idle=>
-                    -- Configuraciónes
-                    case( Speed ) is
-                        when quarter =>
-                            pulse_width_reg <= pulse_width_default*4 - 1;
-                        
-                        when half =>
-                            pulse_width_reg <= pulse_width_default*2 - 1;
-
-                        when normal =>
-                            pulse_width_reg <= pulse_width_default - 1;
-
-                        when doble =>
-                            pulse_width_reg <= pulse_width_default/2 - 1;
-
-                        when others =>
-                    end case ;
-
-                    with N_bits select
-                    N_bits_reg <= 
+                    -- Solo se admite un cambio en velocidad en el estado de idle.
+                    Speed_reg <= Speed;
                     if start='1' then
                         current_state_reg <= StartBit;
                         pulse_count_reg   <= (others=>'0');
@@ -69,7 +54,7 @@ begin
                     if pulse_count_reg = "00000000" then
                         TX_reg          <= '0';
                         pulse_count_reg <= pulse_count_reg + 1;
-                    elsif pulse_count_reg = pulse_width_reg then
+                    elsif pulse_count_reg = pulse_width then
                         current_state_reg <= SendData;
                         pulse_count_reg   <= (others=>'0');
                         data_count_reg    <= (others=>'0');
@@ -81,7 +66,7 @@ begin
                     if pulse_count_reg = "00000000" then
                         TX_reg          <= data(to_integer(data_count_reg));
                         pulse_count_reg <= pulse_count_reg + 1; 
-                    elsif pulse_count_reg = pulse_width_reg then
+                    elsif pulse_count_reg = pulse_width then
                         pulse_count_reg <= (others=>'0');
                         if data_count_reg = "111" then
                             current_state_reg <= StopBit;
@@ -97,7 +82,7 @@ begin
                         TX_reg          <= '1';
                         data_count_reg  <= (others=>'0');
                         pulse_count_reg <= pulse_count_reg + 1;
-                    elsif pulse_count_reg = pulse_width_reg then
+                    elsif pulse_count_reg = pulse_width then
                         current_state_reg <= idle;
                         pulse_count_reg   <= (others=>'0');
                     else
@@ -109,7 +94,28 @@ begin
     end if;
 end process;
 
-    -- Registro de EOT (No tiene que ser registrado, podemos ponerlo como l�gica combinacional, pero as� habr� glitches y supongo que eso no lo queremos. Por otro lado nos deja quitar registros, as� haciendo que sea m�s r�pido y ocupe menos espacio. Con el consumo de potencia no s� si va a consumir m�s o menos, eso depender�a del resto del circuito. Tambi�n supongo que no tenerlo registrado va a hacer el timing dificil, y ser�a mejor pr�ctica tenerlo registrado.) 
+-- Multiplexor para decidir a qué pulse length contar, Speed_reg no cambia fuera de idle
+pulse_width_comb:process(Speed_reg)
+begin
+    case( Speed_reg ) is
+        when quarter =>
+            pulse_width <= pulse_width_default*4 - 1;
+        
+        when half =>
+            pulse_width <= pulse_width_default*2 - 1;
+
+        when normal =>
+            pulse_width <= pulse_width_default - 1;
+
+        when doble =>
+            pulse_width <= pulse_width_default/2 - 1;
+
+        when others =>
+            pulse_width <= pulse_width_default - 1;
+    end case ;
+end process
+
+-- Registro de EOT (No tiene que ser registrado, podemos ponerlo como l�gica combinacional, pero as� habr� glitches y supongo que eso no lo queremos. Por otro lado nos deja quitar registros, as� haciendo que sea m�s r�pido y ocupe menos espacio. Con el consumo de potencia no s� si va a consumir m�s o menos, eso depender�a del resto del circuito. Tambi�n supongo que no tenerlo registrado va a hacer el timing dificil, y ser�a mejor pr�ctica tenerlo registrado.) 
 EOT_process:PROCESS(clk)
 BEGIN
     IF rising_edge(clk) THEN
@@ -125,8 +131,8 @@ BEGIN
     END IF;
 END PROCESS;
         
-    -- Outputs:
-    TX  <= TX_reg;
-    EOT <= EOT_reg;
+-- Outputs:
+TX  <= TX_reg;
+EOT <= EOT_reg;
 
 end Behavioral;
