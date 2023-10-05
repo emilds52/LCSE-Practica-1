b5fe79dd92e6db15f76947d2134d6c0637f877f8
emilds52 <emilds@live.com>
2023-10-05 16:39:27 +0200

    RX done

--- P_RS232.srcs/sources_1/new/RS232_RX.vhd
+++ P_RS232.srcs/sources_1/new/RS232_RX.vhd
@@ -1,7 +1,8 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
 use IEEE.NUMERIC_STD.ALL;
+library util;
+use util.utility.ALL;
 
 
 entity RS232_RX is
@@ -9,6 +10,8 @@ port (
     Clk       : in  std_logic;
     Reset     : in  std_logic;
     LineRD_in : in  std_logic;
+    Speed     : in  speed_t;
+    N_bits    : in  nbits_t;
     Valid_out : out std_logic;
     Code_out  : out std_logic;
     Store_out : out std_logic
@@ -24,21 +27,27 @@ architecture Behavioral of RS232_RX is
     signal Code_tmp  : std_logic;
     signal Store_tmp : std_logic;
     -- Internos
-    constant pulse_width     : unsigned(7 downto 0):=to_unsigned(174,8);
-    signal   bitCounter_reg  : unsigned(7 downto 0);
-    signal   bitCounter_tmp  : unsigned(7 downto 0);
-    signal   data_count_reg  : unsigned(2 downto 0);
-    signal   data_count_tmp  : unsigned(2 downto 0);
-    signal   stopFlag_reg    : std_logic;
-    signal   stopFlag_tmp    : std_logic;
+    constant pulse_width_default : unsigned(7 downto 0):=to_unsigned(174,8);
+    signal   pulse_width         : unsigned(9 downto 0);
+    signal   bitCounter_reg      : unsigned(9 downto 0);
+    signal   bitCounter_tmp      : unsigned(9 downto 0);
+    signal   word_length         : unsigned(2 downto 0);
+    signal   data_count_reg      : unsigned(2 downto 0);
+    signal   data_count_tmp      : unsigned(2 downto 0);
+    signal   stopFlag_reg        : std_logic;
+    signal   stopFlag_tmp        : std_logic;
+    signal   Speed_reg           : speed_t;
+    signal   Speed_tmp           : speed_t;
+    signal   N_bits_reg          : nbits_t;
+    signal   N_bits_tmp          : nbits_t;
+
 
     type state is (idle, StartBit, RcvData, StopBit);
     signal current_state_reg : state;
     signal next_state        : state;
-
-    begin
-
-    comb:process(LineRD_in, current_state_reg, reset, bitCounter_reg, stopFlag_reg, data_count_reg) --process(all) da error de sintaxis no soportado desde 1076-2008
+begin
+    
+    comb:process(LineRD_in, current_state_reg, bitCounter_reg, stopFlag_reg, data_count_reg, Speed, N_bits) --process(all) da error de sintaxis no soportado desde 1076-2008
     begin
         -- Valores por defecto
         next_state     <= current_state_reg;
@@ -48,19 +57,24 @@ architecture Behavioral of RS232_RX is
         Valid_tmp      <= '0';
         Code_tmp       <= '0';
         Store_tmp      <= '0';
+        Speed_tmp      <= Speed_reg;
+        N_bits_tmp     <= N_bits_reg;
         
         case(current_state_reg) is
         
             when idle =>
                 data_count_tmp <= (others => '0'); -- Creo que no hace falta poner esto a cero, ya que el rst lo hace y se hace al salir de stopbit
                 stopFlag_tmp   <= '0';
+                -- Solo actualizar la velocidad de transmisón y el número de bits en el estado de idle
+                Speed_tmp <= Speed;
+                N_bits_tmp <= N_bits;
                 if LineRD_in = '0' then
                     next_state <= StartBit;
                 end if;
             
             when StartBit =>
                 -- Contar hasta medio pulso para muestrar en el centro
-                if bitCounter_reg = pulse_width/2 - 1 then
+                if bitCounter_reg = ('0' & pulse_width(9 downto 1)) then -- Un bitshift para dividir entre 2
                     bitCounter_tmp <= (others=>'0');
                     next_state     <= RcvData;
                 else 
@@ -68,10 +82,10 @@ architecture Behavioral of RS232_RX is
                 end if;  
             
             when RcvData =>
-                if bitCounter_reg = pulse_width - 1 then
+                if bitCounter_reg = pulse_width then
                     Valid_tmp <= '1';
                     Code_tmp  <= LineRD_in;
-                    if data_count_reg /= to_unsigned(7,data_count_reg'length) then
+                    if data_count_reg /= word_length then -- Not equals is cheaper than less than (pedantic)
                         bitCounter_tmp <= (others=>'0');
                         data_count_tmp <= data_count_reg + 1;
                     else
@@ -82,11 +96,11 @@ architecture Behavioral of RS232_RX is
                 end if;
                     
             when StopBit =>
-                if (bitCounter_reg = pulse_width - 1 and stopFlag_reg='0') then
+                if (bitCounter_reg = pulse_width and stopFlag_reg='0') then
                     Store_tmp      <= LineRD_in;
                     bitCounter_tmp <= (others=>'0');
                     stopFlag_tmp   <= '1';
-                elsif (bitCounter_reg = pulse_width/2 - 1 and stopFlag_reg='1') then
+                elsif (bitCounter_reg = ('0' & pulse_width(9 downto 1)) and stopFlag_reg='1') then -- bitshift para dividir por dos
                     data_count_tmp <= (others => '0');
                     bitCounter_tmp <= (others => '0');
                     stopFlag_tmp   <= '0';
@@ -100,8 +114,51 @@ architecture Behavioral of RS232_RX is
                 
         end case ;
     end process;
+    
+    -- Multiplexor para decidir a qué pulse length contar, Speed_reg no cambia fuera de idle
+    pulse_width_comb:process(Speed_reg)
+    begin
+        case( Speed_reg ) is
+            when quarter =>
+                pulse_width <= resize(pulse_width_default*4 - 1,pulse_width'length);
 
-    reg : process( clk, reset )
+            when half =>
+                pulse_width <= resize(pulse_width_default*2 - 1,pulse_width'length);
+
+            when normal =>
+                pulse_width <= resize(pulse_width_default - 1,pulse_width'length);
+
+            when doble =>
+                pulse_width <= resize(pulse_width_default/2 - 1,pulse_width'length);
+
+            when others =>
+                pulse_width <= resize(pulse_width_default - 1,pulse_width'length);
+        end case;
+    end process;
+
+
+    -- Multiplexor para decidir el numero de bits
+    N_bits_comb:process(N_bits_reg)
+    begin
+        case( N_bits_reg ) is
+            when fiveBits =>
+                word_length <= to_unsigned(4,3);
+
+            when sixBits =>
+                word_length <= to_unsigned(5,3);
+
+            when sevenBits =>
+                word_length <= to_unsigned(6,3);
+
+            when eightBits =>
+                word_length <= to_unsigned(7,3);
+
+            when others =>
+                word_length <= to_unsigned(7,3); -- por si acaso
+        end case;
+    end process;
+
+    regs : process( clk, reset )
     begin
         if reset='0' then
             bitCounter_reg    <= (others => '0');
@@ -111,6 +168,8 @@ architecture Behavioral of RS232_RX is
             Code_reg          <= '0';
             Store_reg         <= '0';
             current_state_reg <= idle;
+            Speed_reg         <= normal;
+            N_bits_reg        <= eightBits;
         elsif rising_edge(clk) then
             bitCounter_reg    <= bitCounter_tmp;
             data_count_reg    <= data_count_tmp;
@@ -119,9 +178,12 @@ architecture Behavioral of RS232_RX is
             Code_reg          <= Code_tmp;
             Store_reg         <= Store_tmp;
             current_state_reg <= next_state;
-        end if ;
+            Speed_reg         <= Speed_tmp;
+            N_bits_reg        <= N_bits_tmp;
+        end if;
     end process;
     
+    -- Salidas
     Valid_out <= Valid_reg;
     Code_out  <= Code_reg;
     Store_out <= Store_reg;
