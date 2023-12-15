351fec22bcdfac645e284fe08ed5d650fab2c561
Vml312 <valentinmtnz99@gmail.com>
2023-12-15 17:23:17 +0100

    Otro intento BCD

--- P_RS232.srcs/sources_1/new/SplitDig.vhd
+++ P_RS232.srcs/sources_1/new/SplitDig.vhd
@@ -17,7 +17,7 @@ architecture Behavioral of SplitDig is
 
 signal Data_i_aux : unsigned(Data_i'range);
 signal resto : unsigned(Data_i'range);
-signal flag_primera_ex : boolean;
+signal i : integer range 0 to 15;
 begin
 
 process(CLK, reset)
@@ -26,33 +26,37 @@ begin
         Data_o <= (others=>'0');
     elsif rising_edge(clk) then
         if enable='0' then  
-          flag_primera_ex <= true;
+          i <= 15;
           Data_i_aux <= (others =>'0');
           resto  <= (others=>'0');
-          flag_primera_ex <= true;
         else
-          if flag_primera_ex then
+          if i = 15 then
             reverse_gen: for i in 0 to data_i'length-1 loop
               data_i_aux(i) <= data_i(data_i'length-1 - i);
             end loop;
-            flag_primera_ex <= false;
-          else
-            --división de 8 a 0 en tres vectores de 4 elementos                    
-            for i in 0 to 6 loop
-              if Data_i_aux(3+4*i downto 4*i) + resto > 9 then
-                Data_o(Data_o'length-1 -4*i downto Data_o'length-1 -3-4*i) <= std_logic_vector(to_unsigned(9,4));
-                Data_i_aux <= resize(Data_i_aux - 9, Data_i_aux'length);
-                resto <= resize(resto + Data_i_aux(3+4*i downto 4*i) - 9, resto'length);
-              else
-                Data_o(Data_o'length-1 -4*i downto Data_o'length-1 -3-4*i) <= std_logic_vector(resize(Data_i_aux(3+4*i downto 4*i) + resto, 4));
-                Data_i_aux <= resize(Data_i_aux - Data_i_aux(3+4*i downto 4*i) - resto, Data_i_aux'length);
-                resto <= (others=>'0');
-              end if;
-            end loop;
-            Data_o(31 downto 28) <= std_logic_vector(resize(Data_i_aux + resto, 4));
+            i <= 0;
+          end if;
+          if i < 8 then
+            if i = 0 or i <= 6 then
+              --división de 8 a 0 en tres vectores de 4 elementos                    
+                if Data_i_aux(3+4*i downto 4*i) + resto > 9 then
+                  Data_o(Data_o'length-1 -4*i downto Data_o'length-1 -3-4*i) <= std_logic_vector(to_unsigned(9,4));
+                  Data_i_aux <= resize(Data_i_aux - 9, Data_i_aux'length);
+                  resto <= resize(resto + Data_i_aux(3+4*i downto 4*i) - 9, resto'length);
+                else
+                  Data_o(Data_o'length-1 -4*i downto Data_o'length-1 -3-4*i) <= std_logic_vector(resize(Data_i_aux(3+4*i downto 4*i) + resto, 4));
+                  Data_i_aux <= resize(Data_i_aux - Data_i_aux(3+4*i downto 4*i) - resto, Data_i_aux'length);
+                  resto <= (others=>'0');
+                end if;
+                i <= i + 1;
+            else
+              Data_o(3 downto 0) <= std_logic_vector(resize(Data_i_aux + resto, 4));
+              resto <= (others => '0');
+              i <= 12;
+            end if;
           end if;
         end if;
-    end if;
+      end if;
 end process;
 
 end Behavioral;
